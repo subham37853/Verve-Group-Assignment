381a3488757704a370e333d3125f1117f5d4c104
Shubham Prasad <37408670+subham37853@users.noreply.github.com>
2024-11-24 23:06:55 +0530

    Add files via upload

--- src/main/java/com/verve_group/assignment/init/AssignmentApplication.java
+++ src/main/java/com/verve_group/assignment/init/AssignmentApplication.java
@@ -3,8 +3,10 @@ package com.verve_group.assignment.init;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @ComponentScan(basePackages = "com.verve_group.assignment")
 public class AssignmentApplication {
 
